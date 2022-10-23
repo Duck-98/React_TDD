1e42b73f9120c5c134a8d2c672608051a0207577
duck <godqhr2256@gmail.com>
2022-10-23 23:10:35 +0900

    디렉토리 구조 세팅

---
+++ src/pages/CompletePage/index.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function CompletePage() {
+  return <div>index</div>;
+}
+
+export default CompletePage;
---
+++ src/pages/OrderPage/index.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function OrderPage() {
+  return <div>index</div>;
+}
+
+export default OrderPage;
